8b9b5f808360dd77d573f9c34544924130ec3dd7
KlausMikhaelson <satyamsingh5076@gmail.com>
2022-11-11 19:26:33 +0530

    done with the mvvp

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const form =  document.getElementById("main");
+const inputName = document.getElementById("input");
+const todoslist = document.getElementById("todolist")
 
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const todo = inputName.value;
+
+    if(todo) {
+        const todoText = document.createElement("li")
+        todoText.innerText =  todo;
+        todoslist.appendChild(todoText)
+    }
+})
